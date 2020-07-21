1412e240b232be951f1706ac54f8bb88c2ec468e
Prasanta Kakati <prasantakakati1994@gmail.com>
2020-07-22 01:29:10 +0530

    [ADDED] Matomo integration

--- website/config.js
+++ website/config.js
@@ -1,25 +1,26 @@
-var api = 'http://localhost:8117'
+var api = "https://pool.baza.foundation/api/v2";
 
-var api_blockexplorer = 'http://localhost:5000/api/v1'
+var api_blockexplorer = "https://pool.baza.foundation/api/v1";
 
-var poolHost = 'pool.baza.foundation'
+var poolHost = "pool.baza.foundation";
 
-var irc = ''
+var irc = "";
 
-var email = 'support@baza.foundation'
+var email = "support@baza.foundation";
 
-var telegram = 'https://t.me/bazafoundation'
+var telegram = "https://t.me/bazafoundation";
 
-var cryptonatorWidget = ['{symbol}-BTC', '{symbol}-USD', '{symbol}-EUR']
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
-var easyminerDownload = 'https://github.com/zone117x/cryptonote-easy-miner/releases/'
+var easyminerDownload =
+    "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = 'https://explorer.baza.foundation/?hash={id}#blockchain_block'
+var blockchainExplorer =
+    "https://explorer.baza.foundation/?hash={id}#blockchain_block";
 
-var transactionExplorer = 'https://explorer.baza.foundation/?hash={id}#blockchain_transaction'
+var transactionExplorer =
+    "https://explorer.baza.foundation/?hash={id}#blockchain_transaction";
 
-var themeCss = 'themes/baza-theme.css'
+var themeCss = "themes/baza-theme.css";
 
-var networkStat = {
-  
-}
+var networkStat = {};
